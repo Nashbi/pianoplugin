b18c5e74ea40f1888b26ed46c23d459e550adecb
Nashbi Grand-Jean <nhgrandjean1@gmail.com>
2022-07-22 19:10:33 -0400

    pianoplugin

--- assets/script.js
+++ assets/script.js
@@ -1,12 +1,34 @@
+const wKeyboardNotes = ['z', 'x', 'c', 'v', 'b', 'n', 'm', ',']
+const bKeyboardNotes = ['s', 'd', 'g', 'h', 'j']
+
 const keys = document.querySelectorAll('.key')
+const whiteKeys = document.querySelectorAll('.key.white')
+const blackKeys = document.querySelectorAll('.key.black')
 
 keys.forEach(key => {
-key.addEventListener('click', () => playNote(key))
+key.addEventListener('click', () => playNote(key)) 
 })
+
+document.addEventListener('keydown', e => {
+    if (e.repeat) return
+    const key = e.key
+    const whiteKeyIndex = wKeyboardNotes.indexOf(key)
+    const blackKeyIndex = bKeyboardNotes.indexOf(key) 
+
+    if (whiteKeyIndex > -1) playNote (whiteKeys[whiteKeyIndex])
+    if (blackKeyIndex > -1) playNote (blackKeys[blackKeyIndex])
+
+
+})
+
 
 function playNote(key) {
     const noteAudio = document.getElementById(key.dataset.note)
     noteAudio.currentTime = 0
     noteAudio.play()
-    key.classList
+    key.classList.add('active')
+    noteAudio.addEventListener('ended', () => {
+    key.classList.remove('active')
+    })
+
 }
